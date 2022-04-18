5ca7991d5fd69a40e0c588ec6d9c224ce11f5376
Bakhtyiar Klimov <baxtiar00@gmail.com>
2022-04-19 01:33:09 +0300

    v:0.1.0 Bootstrap added, some sections developed, without adaptivity, some stretch

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import '@root/src/index.html';
+import '@root/node_modules/bootstrap/dist/js/bootstrap';
+import '@root/node_modules/bootstrap/dist/css/bootstrap.css';
+import '@root/src/scss/style.scss';
+
+// let path = require.context('@assets/img/first_section', true, /\.(png|svg|jpg|jpeg|webm|gif)$/);
+// path = path.keys().map(path);
+// console.log(path);
+// const firstSectionImages = document.querySelectorAll('.first-section__image');
+// firstSectionImages.forEach((img, index) => {
+// 	img.src = path[index];
+// });
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		main: './index.js'
+	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[name].[hash][ext]'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.css$/i,
+				use: [
+					"style-loader", 
+					"css-loader",
+					"postcss-loader",
+				]
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+				"style-loader",
+				"css-loader",
+				"sass-loader",
+				]			
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|webm|gif)$/,
+				type: 'asset/resource'
+			},
+			{
+				test: /\.(ttf|otf|eot|woff|woff2)$/,
+				use: [
+					'file-loader'
+				],
+				type: 'javascript/auto'
+			}
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './index.html'
+		}),
+		new CleanWebpackPlugin()
+	],
+	resolve: {
+		alias: {
+			'@root': path.resolve(__dirname),
+			'@assets': path.resolve(__dirname, 'src/assets/')
+		}
+	},
+	devtool: 'source-map',
+	devServer: {
+		hot: true,
+		static: {
+			directory: path.join(__dirname, 'src'),
+			publicPath: '/serve-public-path-url',
+		  },
+		compress: true,
+		port: 9000,
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
+	}
+};
