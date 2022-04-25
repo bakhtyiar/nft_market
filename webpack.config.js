da24ff7bb0b67dfb37ad00e0fd97ea4646bce153
Bakhtyiar Klimov <baxtiar00@gmail.com>
2022-04-25 15:47:13 +0300

    v0.1.4 Added adaptivity to first section, header, partners sections, fixed some lacks of colleciton section margins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
@@ -52,7 +51,7 @@ module.exports = {
 		new HTMLWebpackPlugin({
 			template: './index.html'
 		}),
-		new CleanWebpackPlugin()
+		new CleanWebpackPlugin(),
 	],
 	resolve: {
 		alias: {
